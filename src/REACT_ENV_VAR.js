a5e351c2bf380330e5004db9d47f2a5592319a19
Dave Istanto <daveistanto@Daves-Machine.local>
2019-12-11 14:03:33 -0600

    changed db driver address

--- src/REACT_ENV_VAR.js
+++ src/REACT_ENV_VAR.js
@@ -1,7 +1,7 @@
 // Variable needed by components and react
 
 const GENOME_LIST = ['Select from List', 'Sorghum'] // Used in DropdownGenome.js component
-const DB_DRIVER_ADDRESS = 'http://ec2-3-135-185-199.us-east-2.compute.amazonaws.com:4000' // Used in callQuery.js
+const DB_DRIVER_ADDRESS = 'http://ec2-13-58-30-80.us-east-2.compute.amazonaws.com:4000' // Used in callQuery.js
 const CRISPR_SYSTEMS = {cas9: 'S. pyogenes Cas9'} //cpf1: 'Prevotella/Francisella Cpf1'}
 
 export { GENOME_LIST, DB_DRIVER_ADDRESS, CRISPR_SYSTEMS }
